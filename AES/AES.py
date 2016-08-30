80ff1bb1c8e8be035762eff815bf12790d2a0056
João Marcos Campagnolo <Joo Marcos Campagnolo>
2016-08-30 20:16:31 -0300

    Programa criptografando e descriptografando a mensagem.

--- AES/AES.py
+++ AES/AES.py
@@ -1,6 +1,17 @@
 from Crypto.Cipher import AES
 
 # Variáveis do Programa:
-text = "A mensagem de 16"
+message = "A mensagem de 16"
 IV = "Criptograficando"
 key = "1234567891098765"
+
+# Criptografando a mensagem:
+obj1 = AES.new(key, AES.MODE_CBC, IV)
+ciphertext = obj1.encrypt(message)
+print(ciphertext)
+
+# Descriptografando a mensagem:
+obj2 = AES.new(key, AES.MODE_CBC, IV)
+deciphertext = obj2.decrypt(ciphertext)
+newMessage = deciphertext.decode()
+print(newMessage)
