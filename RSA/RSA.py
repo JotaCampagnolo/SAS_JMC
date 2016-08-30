a17e810225c21677d9892f1ac8d2102f9368f34e
João Marcos Campagnolo <Joo Marcos Campagnolo>
2016-08-30 20:30:24 -0300

    Geração das chaves privada e pública.

--- RSA/RSA.py
+++ RSA/RSA.py
@@ -1,3 +1,8 @@
 import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
+
+# Geração da Chave Privada e da Chave Pública:
+randomGenerator = Random.new().read
+privateKey = RSA.generate(1024, randomGenerator)
+publicKey = privateKey.publickey()
